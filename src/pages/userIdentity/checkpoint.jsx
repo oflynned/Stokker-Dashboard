47c365a8cd43caed57a75fad37833b908e2b0196
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-28 19:26:27 +0100

    add user identity checkpoints in router

---
+++ src/pages/userIdentity/checkpoint.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import LoadingScreen from 'react-loading-screen';
+import { getKey, isSessionValid, KEY_SESSION_EXPIRY } from '../../common/session/localCache';
+import { fetchApi } from '../../common/network/api';
+import { authEndpoint } from '../../common/network/endpoints';
+import { Redirect } from 'react-router';
+
+class Checkpoint extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      hasValidSession: false
+    };
+  }
+
+  async checkRemoteSessionValidity() {
+    try {
+      const session = await fetchApi('GET', authEndpoint);
+      console.log(session)
+      // TODO recache the expiry time
+      this.setState({
+        isLoading: false,
+        hasValidSession: true
+      });
+    } catch (err) {
+      console.error(err);
+      this.setState({
+        isLoading: false,
+        hasValidSession: false
+      });
+    }
+  }
+
+  componentDidMount() {
+    if (isSessionValid()) {
+      this.setState({
+        isLoading: false,
+        hasValidSession: true
+      });
+    } else {
+      this.checkRemoteSessionValidity();
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <LoadingScreen
+        loading={this.state.isLoading}
+        bgColor="#FBF5F3"
+        spinnerColor="#EA0232"
+        textColor="#EA0232"
+        logoSrc={require('../../assets/reactLogo.png')}/>;
+    }
+
+    return this.state.hasValidSession ?
+      <Redirect to={'/dashboard'}/> :
+      <Redirect to='/login'/>;
+  }
+}
+
+export default Checkpoint;
--- src/pages/userIdentity/login.jsx
+++ src/pages/userIdentity/login.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { fetchAccount } from '../../common/network/api';
 
-import { KEY_ID, KEY_NAME, KEY_SESSION, saveKey } from '../../common/session/localCache';
-import { fetchNewAccount } from '../../common/network/api';
+import Button from '@material/react-button';
+import TextField, { Input } from '@material/react-text-field';
+
+import { Redirect } from 'react-router';
+import {
+  isSessionValid,
+  KEY_ID,
+  KEY_NAME,
+  KEY_SESSION,
+  KEY_SESSION_EXPIRY,
+  saveKey
+} from '../../common/session/localCache';
 
 import './login.css';
-import { Redirect } from 'react-router';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isRedirecting: false
+      isRedirecting: false,
+      email: '',
+      password: ''
     };
+  }
+
+  componentDidMount() {
+    if (isSessionValid()) {
+      this.setState({ isRedirecting: true });
+    }
   }
 
   render() {
@@ -21,23 +39,44 @@ class Login extends Component {
 
     return (
       <div className={'login'}>
-        <div className={'click-me'} onClick={() => this.onButtonClick()}>
-          Click me
+        <div className={'login-content'}>
+          <h1>Login to Stokker</h1>
+
+          <TextField className="field" label="Email">
+            <Input
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.currentTarget.value })}/>
+          </TextField>
+
+          <TextField className="field" label="Password">
+            <Input
+              type={'password'}
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.currentTarget.value })}/>
+          </TextField>
+
+          <Button raised onClick={() => this.onButtonClick()}>
+            Login
+          </Button>
         </div>
       </div>
     );
   }
 
   onButtonClick() {
-    fetchNewAccount({ name: 'Ed' }, 'oflynned@gmail.com', 'password1')
-      .then(({ data: { session, user: { _id, name } } }) => {
+    fetchAccount(this.state.email, this.state.password)
+      .then(({ data: { rawSessionId, session: { expiryTime, user: { _id, name } } } }) => {
+
         // cache the session returned from the API first
-        saveKey(KEY_SESSION, session);
-        saveKey(KEY_ID, _id);
+        saveKey(KEY_SESSION, rawSessionId);
         saveKey(KEY_NAME, name);
+        saveKey(KEY_SESSION_EXPIRY, expiryTime);
+        saveKey(KEY_ID, _id);
+
+        this.setState({ isRedirecting: true });
 
         // now forcibly redirect the user to the dashboard
-        this.props.setAuthState({ hasVerifiedSession: true });
+        // this.props.setAuthState({ hasVerifiedSession: true });
       })
       .catch(err => console.log(err));
   }
